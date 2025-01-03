1c271031d1346a65ea3b9e363f9b98a9bbca3db3
Juan Carlos <juannnerysantos@gmail.com>
2025-01-02 23:13:09 -0300

    chore: add express configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import express, { json, Request, Response } from "express";
+
+const app = express();
+app.use(json());
+const port = process.env.PORT;
+
+app.get("/", async (req: Request, res: Response) => {
+  res.json({ player: "Juan" });
+});
+
+app.listen(port, () => {
+  console.log(`Server runnig at port http://localhost:${port}`);
+});

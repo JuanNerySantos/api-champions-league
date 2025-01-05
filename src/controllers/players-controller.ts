26584ace08c0eb3b35a079fba9b428da5913fa29
Juan Carlos <juannnerysantos@gmail.com>
2025-01-05 17:17:49 -0300

    chore: create layer Service

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
+import { getPlayerService } from "../services/players-service";
 
 export const getPlayer = async (req: Request, res: Response) => {
-  res.json({ player: "iii" });
+  const responseService = await getPlayerService();
+  res.json(responseService);
 };

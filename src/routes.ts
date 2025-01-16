acc205a8ed8ebff5f1bfd169924c8d262555ec1b
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-15 23:33:01 -0300

    feat: create router from Delete Player

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createPlayer,
+  DeletePlayerById,
   getPlayerById,
   getPlayers,
 } from "./controllers/players-controller";
@@ -10,5 +11,6 @@ const router = Router();
 router.get("/players", getPlayers);
 router.get("/players/:id", getPlayerById);
 router.post("/players/", createPlayer);
+router.delete("/players/:id", DeletePlayerById);
 
 export default router;

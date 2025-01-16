0b038ccb3dd8f4d81ced7834ec5ccceab3ae05d0
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-15 23:33:46 -0300

    feat: create controller from Delete Player

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import {
   createPlayerService,
+  DeletePlayerByIdService,
   getPlayerByIdService,
   getPlayerService,
 } from "../services/players-service";
@@ -21,6 +22,14 @@ export const getPlayerById = async (req: Request, res: Response) => {
 export const createPlayer = async (req: Request, res: Response) => {
   const player = req.body;
   const httpResponse = await createPlayerService(player);
+
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+};
+
+export const DeletePlayerById = async (req: Request, res: Response) => {
+  const playerId = parseInt(req.params.id);
+
+  const httpResponse = await DeletePlayerByIdService(playerId);
 
   res.status(httpResponse.statusCode).json(httpResponse.body);
 };

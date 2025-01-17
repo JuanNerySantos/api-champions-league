6b04e67740f822346f424a1bf19a4d5fc85008d5
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-16 23:48:27 -0300

    feat: create controller from update Player

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -14,6 +14,7 @@ export const getPlayers = async (req: Request, res: Response) => {
 
 export const getPlayerById = async (req: Request, res: Response) => {
   const id = parseInt(req.params.id);
+
   const httpResponse = await getPlayerByIdService(id);
 
   res.status(httpResponse.statusCode).json(httpResponse.body);
@@ -21,6 +22,7 @@ export const getPlayerById = async (req: Request, res: Response) => {
 
 export const createPlayer = async (req: Request, res: Response) => {
   const player = req.body;
+
   const httpResponse = await createPlayerService(player);
 
   res.status(httpResponse.statusCode).json(httpResponse.body);
@@ -30,6 +32,16 @@ export const DeletePlayerById = async (req: Request, res: Response) => {
   const playerId = parseInt(req.params.id);
 
   const httpResponse = await DeletePlayerByIdService(playerId);
+
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+};
+
+export const UpdatePlayer = async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id);
+
+  const updateParams = req.body;
+
+  const httpResponse = await UpdatePlayerService(id, updateParams);
 
   res.status(httpResponse.statusCode).json(httpResponse.body);
 };

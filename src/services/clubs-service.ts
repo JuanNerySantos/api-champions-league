d387b6edfb1b1cce1e6307a1ccc9fa3bb1dd1ff5
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-18 00:56:20 -0300

    feat: implement service from get club

--- src/services/clubs-service.ts
+++ src/services/clubs-service.ts
@@ -1,1 +1,16 @@
-export const getClubService = () => {};
+import { getClubRepository } from "../repositories/club-repository";
+import { badRequest, notFound } from "../utils/http-response";
+
+export const getClubService = async (id: number) => {
+  if (Object.keys(id).length === 0) {
+    return badRequest();
+  }
+
+  const club = await getClubRepository(id);
+
+  if (!club) {
+    return notFound();
+  }
+
+  return club;
+};

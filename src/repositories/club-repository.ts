af900c1cd405ed6b75ea2edea08c95de399fdb64
JuanNerySantos <juannnerysantos@gmail.com>
2025-01-18 01:10:32 -0300

    feat: implement repository from get club

--- src/repositories/club-repository.ts
+++ src/repositories/club-repository.ts
@@ -1,1 +1,7 @@
-export const getClubRepository = () => {};
+import { ClubModel } from "../models/club-model";
+
+const dataBase: ClubModel[] = [{ id: 1, name: "juan" }];
+
+export const getClubRepository = async (): Promise<ClubModel[]> => {
+  return dataBase;
+};
